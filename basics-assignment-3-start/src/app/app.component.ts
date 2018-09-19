50076c7fdae968effc397efa8417e71f726779e8
Jayce77 <xjaycex@hotmail.com>
2018-09-19 19:49:45 -0400

    finished assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  secretPassword: string = 'tuna';
+  passwords: string[] = ['tuna', 'apple', 'Sparticus', 'billy-bob'];
+  clickValues: string[] = [];
+
+  getPassword(): void {
+    let passwordNumber: number = Math.floor(Math.random() * this.passwords.length);
+    this.secretPassword = this.passwords[passwordNumber];
+  };
+
+  onToggleDisplay(): void {
+    this.getPassword();
+    this.clickValues.push(this.secretPassword);
+  }
+
+  getColor(index: number): string {
+    return index > 4 ? "blue" : "white";
+  }
 }
