d089f1bb9618b554abeada410ac4b583199ab141
Jayce77 <xjaycex@hotmail.com>
2018-09-20 22:44:21 -0400

    added test recipe to recipeList template

--- my-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test', `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI6RNc-TjnsRh7g3LnBLcXvWoi0sc-XzLjO1zkpau_MX0nO1knVw`)
+  ];
   constructor() { }
 
   ngOnInit() {
